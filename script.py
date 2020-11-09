9f3a98c6b69fb41dbfd9af851a18f4ab6d881e4c
Art Peake <ajpeake@gmail.com>
2020-11-08 19:38:39 -0600

    Removed some code

--- script.py
+++ script.py
@@ -1,23 +1,25 @@
-import math
-import os
-import sys
-from os import rename
+# import math
+# import os
+# import sys
+# from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#    greeting = "Hello, {}".format(who_to_greet)
+#    return greeting
 
 
-print(greet("World"))
-print(greet("Arthur"))
+# print(greet("World"))
+# print(greet("Arthur"))
+
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
 
 # name = input("Your name? ")
 # print("Hello, ", name)
